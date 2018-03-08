242810364e0d92840c7573e0f123a2abdf24920c
Mehow <Kaiser.mehow@gmail.com>
2018-03-08 13:25:32 +0000

    non-wav surpat

--- track_guess.py
+++ track_guess.py
@@ -6,18 +6,31 @@ from pydub import AudioSegment
 from sys import argv
 from http.server import HTTPServer, BaseHTTPRequestHandler,SimpleHTTPRequestHandler
 
+track_list = []
+
+def find_music_files(music_dir):
+    file_types = ["wav"]
+    ffmpeg_installed = shutil.which("ffmpeg") is not None
+    if ffmpeg_installed:
+        file_types.extend(["mp3"])
+    global track_list
+    track_list = []
+    for root, subdirs, files in os.walk(music_dir):
+        for file_name in files:
+            if file_name.endswith(tuple(file_types)):
+                track_list.append(os.path.join(root, file_name))
+
 def prepare_track():
-    music_dir = argv[1]
-    track_list = list(glob.iglob(music_dir + '/**/*.wav', recursive=True))
     original_track = random.choice(track_list)
     track_name = original_track.split("/")[-1]
-    track = AudioSegment.from_wav(original_track)
+    file_format = original_track.split(".")[-1]
+    track = AudioSegment.from_file(original_track, file_format)
     length_of_clip = 5*1000
     start_of_clip = random.randrange(len(track-length_of_clip))
     end_of_clip = start_of_clip + length_of_clip
     clip = track[start_of_clip:end_of_clip]
     out_f = open("./clip", 'wb')
-    clip.export(out_f, format='wav')
+    clip.export(out_f, format=file_format)
     #todo: send an extedned clip (10sec either side, instead of whole thing?)
     shutil.copy(original_track, "./track")
     with open("./index.template.html", "rt") as template:
@@ -31,7 +44,7 @@ class S(BaseHTTPRequestHandler):
         if self.path in ["/clip", "/track"]:
             self.send_response(200)
             self.send_header('Cache-Control', 'no-cache')
-            self.send_header('Content-type', 'audio/wav')
+            self.send_header('Content-type', 'audio/*')
             self.end_headers()
             if self.path == '/clip' :
                 f = open('./clip', 'rb')
@@ -56,4 +69,6 @@ def run(server_class=HTTPServer, handler_class=S):
     httpd.serve_forever()
 
 if __name__ == "__main__":
+    find_music_files(argv[1])
+    print("scanning music done, starting server")
     run()
